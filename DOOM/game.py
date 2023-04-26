c191354a2decb1163c4c82d225a7837f1b75d37e
Kirk-KD <kirk.ding123@gmail.com>
2023-04-25 21:00:50 -0400

    documentations

--- DOOM/game.py
+++ DOOM/game.py
@@ -1,17 +1,52 @@
+"""The representation of the Game.
+
+This file defines the main `Game` class, and should only be used for 
+the `Game` object from within `main.py`.
+"""
+
+# import pygame
 import pygame as pg
 
+# import all the rendering classes
 from renderer.hud_renderer import HUDRenderer
-from config import *
 from renderer.raycasting import Raycasting
-from player import Player
-from level import Level
 from renderer.object_renderer import ObjectRenderer
 from renderer.objects_manager import ObjectsManager
+
+# import configurations
+from config import *
+
+# all other objects
+from player import Player
+from level import Level
 from audio import AudioManager
 from weapon import *
 
 
 class Game:
+    """An object that handles the initializing and running of the main game loop.
+
+    The `Game` object is responsible for initializing the pygame library by 
+    calling `pg.init()`, and also initializing all the objects used by the game, 
+    such as the player and the level. The only public method that should be called 
+    from the outside is `run()`. The `run` method should be only called from 
+    `main.py`.
+
+    Attributes:
+        `title`: The game window's title.
+        `clock`: Pygame Clock that is responsible for FPS and deltatime calculation.
+        `deltatime`: Keep units consistent under different FPS.
+        `running`: A flag that indicates whether or not the game is running.
+        `surface`: Main window to draw the game on.
+        `audio_manager`: Controls the music and plays sound effects.
+        `hud_renderer`: Renders the HUD (heads up display).
+        `player`: Represents the player.
+        `object_renderer`: Renders most things, including walls, sky, ground, and sprites.
+        `objects_manager`: Stores information about all objects such as enemies and sprites.
+        `level`: Loads the level from a text file and a png file.
+        `raycast`: Cast 2D rays that can be interpreted to create the pseudo-2D style walls.
+    """
+
     def __init__(self, title: str) -> None:
         self.title: str = title
 
--- DOOM/main.py
+++ DOOM/main.py
@@ -1,10 +1,18 @@
+"""Main entry point of the game."""
+
+# import the main Game object forom game.py
 from game import Game
 
 
 def main() -> None:
+    """Main entry point function."""
+
+    # initialize the game
     doom = Game("DOOOOOOM")
+    # run the game
     doom.run()
 
 
+# prevents running if this file is accidentally imported
 if __name__ == "__main__":
     main()
